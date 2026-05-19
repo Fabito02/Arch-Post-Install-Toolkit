61e0e1a1721ed0db8e8fa6cd0073374fdd54d139
Fabiano Júnior Lima Barbosa <138934847+Fabito02@users.noreply.github.com>
2026-05-18 22:40:31 -0300

    Refactor Arch Linux post-installation script and more

--- script_archlinux.sh
+++ script_archlinux.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-sudo -v
-
-if [ "$EUID" -eq 0 ]; then
-  echo "Execute o script como usuário comum."
-  exit 1
-fi
-
-VERDE='\033[0;32m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m'
 MK_CONF="/etc/mkinitcpio.conf"
 LOADER_DIR="/boot/loader/entries"
@@ -17,19 +11,86 @@ CACHE="$HOME/.cache/script_arch"
 PAMAC_RULE_PATH="/etc/polkit-1/rules.d/99-pamac.rules"
 TEMPLATE_DIR=$(xdg-user-dir TEMPLATES)
 
+OPT_NVIDIA=false
+OPT_INTEL=false
+OPT_UNDERVOLT_INTEL=false
+OPT_LOW_RES=false
+
+help() {
+    echo -e "${GREEN}Uso do Script Pós-Instalação Arch Linux${NC}"
+    echo ""
+    echo "Opções:"
+    echo "  -n,  --nvidia             Instala os drivers proprietários da Nvidia"
+    echo "  -i,  --intel              Aplica a otimizações específicas para processadores Intel"
+    echo "  -uv, --undervolt-intel    Aplica undervolt para processadores Intel"
+    echo "  -lr, --low-res            Ajusta tamanho do cursor e UI para telas menores"
+    echo "  -h,  --help               Mostra o menu de ajuda e sai"
+    echo ""
+    echo "Exemplo: ./script.sh -n -i -lr"
+    exit 0
+}
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -n|--nvidia) OPT_NVIDIA=true ;;
+        -i|--intel) OPT_INTEL=true ;;
+        -uv|--undervolt-intel) OPT_UNDERVOLT_INTEL=true ;;
+        -lr|--low-res) OPT_LOW_RES=true ;;
+        -h|--help) help ;;
+        *) echo "Erro: Parâmetro desconhecido: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [ "$EUID" -eq 0 ]; then
+  echo "Execute o script como usuário comum."
+  exit 1
+fi
+
+sudo -v
+
+# --nvidia
+NVIDIA_PKGS=()
+if [ "$OPT_NVIDIA" = true ]; then
+    echo -e "\n${GREEN}Configuração do Driver NVIDIA${NC}"
+    echo "1) Instalar Driver Atual (nvidia-dkms)"
+    echo "2) Instalar Driver Legado (nvidia-580xx-dkms)"
+    read -p "Escolha a versão do driver (1 ou 2): " nv_escolha
+
+    if [ "$nv_escolha" = "1" ]; then
+        NVIDIA_PKGS=(nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings)
+    elif [ "$nv_escolha" = "2" ]; then
+        NVIDIA_PKGS=(nvidia-580xx-dkms nvidia-580xx-utils lib32-nvidia-580xx-utils nvidia-580xx-settings)
+    else
+        echo "Opção inválida. Pulando instalação dos drivers Nvidia."
+    fi
+fi
+
+UV_VAL=""
+if [ "$OPT_UNDERVOLT_INTEL" = true ]; then
+    echo -e "\n${RED}==================================================================================${NC}"
+    echo -e "${RED}AVISO: VALORES MUITO GRANDES PODEM GERAR TRAVAMENTO IMEDIATO E KERNEL PANIC${NC}"
+    echo -e "${RED}==================================================================================${NC}"
+    echo "Faça isso por sua conta e risco. É altamente recomendado que faça testes antes, para garantir que não acabe danificando o seu sistema."
+    read -t 20 -p "Digite o valor em mV (ex: -50) ou Enter para cancelar: " UV_VAL || UV_VAL=""
+    
+    if [[ ! "$UV_VAL" =~ ^-[0-9]+$ ]]; then
+        echo " -> Valor inválido ou vazio. Undervolt desativado."
+        UV_VAL=""
+    fi
+fi
+
 rm -rf "$CACHE"
 mkdir -p "$CACHE"
 
 PKGS_PACMAN=(
     base-devel adw-gtk-theme discord btop steam gamemode mangohud ryujinx 
     android-tools scrcpy faugus-launcher pcsx2 snes9x dolphin-emu 
-    drawing telegram-desktop qbittorrent impression flatpak
-    lact gparted dconf-editor gdm-settings zed ghostty ufw
-    nvidia-580xx-utils nvidia-580xx-dkms lib32-nvidia-580xx-utils 
-    nvidia-580xx-settings linux-zen-headers firefoxpwa
+    drawing telegram-desktop qbittorrent impression flatpak firefoxpwa
+    lact gparted dconf-editor gdm-settings zed ghostty ufw linux-zen-headers
     noto-fonts-cjk noto-fonts-emoji paru zsh zsh-completions 
     switcheroo-control zsh-syntax-highlighting zsh-autosuggestions 
-    npm ffmpegthumbnailer plymouth fastfetch zram-generator
+    npm ffmpegthumbnailer plymouth fastfetch zram-generator tuned tuned-ppd
     bibata-cursor-theme pamac bazaar fuse zen-browser chromium lsfg-vk eden-git 
     extension-manager refine supertuxkart libgda6 geary github-cli endeavour
     ghostty-nautilus valent-git gnome-boxes amberol mangojuice
@@ -42,17 +103,25 @@ PKGS_FLATPAK=(
     com.cassidyjames.clairvoyant io.github.jeffshee.Hidamari 
     com.vysp3r.ProtonPlus it.mijorus.gearlever com.github.tchx84.Flatseal 
     org.nickvision.tubeconverter io.github.vikdevelop.SaveDesktop 
-    net.sourceforge.wxEDID io.missioncenter.MissionCenter 
+    io.missioncenter.MissionCenter io.github.nozwock.Packet
     io.github.diegopvlk.Cine io.github.amit9838.mousam 
     io.github.tobagin.karere com.pojtinger.felicitas.Sessions
-    io.github.nozwock.Packet io.github.fabrialberio.pinapp
+    io.github.fabrialberio.pinapp
 )
 
 PKGS_AUR=(
     gnome-shell-extension-valent-git cemu-bin
 )
 
-echo -e "${VERDE}Configurando Chaotic-AUR...${NC}"
+if [ ${#NVIDIA_PKGS[@]} -gt 0 ]; then
+    PKGS_PACMAN+=("${NVIDIA_PKGS[@]}")
+fi
+
+if [ "$OPT_UNDERVOLT_INTEL" = true ]; then
+    PKGS_PACMAN+=(intel-undervolt)
+fi
+
+echo -e "${GREEN}Configurando Chaotic-AUR...${NC}"
 sudo pacman -S git --noconfirm
 
 if ! pacman -Qi chaotic-keyring &> /dev/null; then
@@ -62,20 +131,20 @@ if ! pacman -Qi chaotic-keyring &> /dev/null; then
     cd "$CACHE"
 fi
 
-echo -e "${VERDE}Atualizando sistema e instalando pacotes pacman e aur...${NC}"
+echo -e "${GREEN}Atualizando sistema e instalando pacotes pacman e aur...${NC}"
 sudo pacman -Syu --needed --noconfirm "${PKGS_PACMAN[@]}"
 
-echo -e "${VERDE}Instalando pacotes Flatpak...${NC}"
+echo -e "${GREEN}Instalando pacotes Flatpak...${NC}"
 flatpak install flathub "${PKGS_FLATPAK[@]}" -y
 
-echo -e "${VERDE}Instalando pacotes AUR...${NC}"
+echo -e "${GREEN}Instalando pacotes AUR...${NC}"
 paru -S --needed --noconfirm "${PKGS_AUR[@]}"
 
-echo -e "${VERDE}Removendo aplicativos não utilizados...${NC}"
-APPS_INSTALADOS=$(pacman -Qq decibels showtime gnome-music gnome-console epiphany gnome-software gnome-weather yelp gnome-user-docs gnome-tour htop 2>/dev/null || true)
+echo -e "${GREEN}Removendo aplicativos não utilizados...${NC}"
+INSTALLED=$(pacman -Qq decibels showtime gnome-music gnome-console epiphany gnome-software gnome-weather yelp gnome-user-docs gnome-tour htop 2>/dev/null || true)
 
-if [ -n "$APPS_INSTALADOS" ]; then
-    echo "$APPS_INSTALADOS" | sudo pacman -Rns - --noconfirm
+if [ -n "$INSTALLED" ]; then
+    echo "$INSTALLED" | sudo pacman -Rns - --noconfirm
 else
     echo " -> Nenhum dos aplicativos alvos está instalado. Pulando remoção."
 fi
@@ -90,7 +159,7 @@ else
     echo "Aviso: Atalho original do Extensions não encontrado. Pulando."
 fi
 
-echo -e "${VERDE}Configurando Ghostty...${NC}"
+echo -e "${GREEN}Configurando Ghostty...${NC}"
 mkdir -p "$HOME/.config/ghostty"
 cat << 'EOF' > "$HOME/.config/ghostty/config"
 theme = light:Adwaita,dark:Adwaita Dark
@@ -112,7 +181,7 @@ revealer.raised.top-bar {
 }
 EOF
 
-echo -e "${VERDE}Configurando ZSH (Pure, History, Plugins)...${NC}"
+echo -e "${GREEN}Configurando ZSH (Pure, History, Plugins)...${NC}"
 sudo chsh -s "$(which zsh)" "$USER"
 mkdir -p "$HOME/.zsh"
 if [ ! -d "$HOME/.zsh/pure" ]; then
@@ -143,7 +212,7 @@ source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zs
 source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 EOF
 
-echo -e "${VERDE}Criando arquivos modelo...${NC}"
+echo -e "${GREEN}Criando arquivos modelo...${NC}"
 
 if [ -d "$TEMPLATE_DIR" ]; then
     touch "$TEMPLATE_DIR/Documento de Texto.txt"
@@ -170,7 +239,7 @@ else
     echo "Aviso: Pasta de modelos não encontrada pelo XDG. Pulando."
 fi
 
-echo -e "${VERDE}Configurando Interface e Temas...${NC}"
+echo -e "${GREEN}Configurando Interface e Temas...${NC}"
 flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark -y
 sudo flatpak override --filesystem=xdg-data/themes
 sudo flatpak override --filesystem=xdg-config/gtk-3.0
@@ -183,21 +252,27 @@ gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface color-scheme 'default'
 gsettings set org.gnome.shell disable-extension-version-validation true
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
-gsettings set org.gnome.desktop.interface cursor-size 20
+gsettings set org.gnome.shell always-show-log-out true
+
+if [ "$OPT_LOW_RES" = true ]; then
+    gsettings set org.gnome.desktop.interface cursor-size 20
+fi
 
 cd "$CACHE"
 git clone --depth 1 https://github.com/maximilionus/lucidglyph
 cd lucidglyph && sudo ./lucidglyph.sh install
 cd "$CACHE"
 
-echo -e "${VERDE}Configurando Plymouth e intel_pstate${NC}"
+echo -e "${GREEN}Configurando Plymouth e Parâmetros do Kernel${NC}"
 sudo sed -Ei '/^HOOKS=/ { /plymouth/! s/(udev)/\1 plymouth/ }' "$MK_CONF"
 
 if [ -d "$LOADER_DIR" ]; then
     for conf in "$LOADER_DIR"/*.conf; do
         [ -f "$conf" ] && grep -q "^options" "$conf" || continue
-
-        grep -q "intel_pstate=passive" "$conf"  || sudo sed -i '/^options/ s/$/ intel_pstate=passive/' "$conf"
+        
+        if [ "$OPT_INTEL" = true ]; then
+            grep -q "intel_pstate=passive" "$conf"  || sudo sed -i '/^options/ s/$/ intel_pstate=passive/' "$conf"
+        fi
         grep -q "quiet" "$conf"  || sudo sed -i '/^options/ s/$/ quiet/' "$conf"
         grep -q "splash" "$conf" || sudo sed -i '/^options/ s/$/ splash/' "$conf"
         
@@ -207,14 +282,14 @@ else
     echo "Diretório $LOADER_DIR não encontrado. Pulando bootloader."
 fi
 
-echo -e "${VERDE}Instalando o tema Plymouth${NC}"
+echo -e "${GREEN}Instalando o tema Plymouth${NC}"
 paru -S --noconfirm plymouth-theme-arch-darwin
 sudo plymouth-set-default-theme -R arch-darwin
 
-echo -e "${VERDE}Habilitando NTSYNC (Para jogos Windows via Proton/Wine)${NC}"
+echo -e "${GREEN}Habilitando NTSYNC (Para jogos Windows via Proton/Wine)${NC}"
 echo "ntsync" | sudo tee /etc/modules-load.d/ntsync.conf
 
-echo -e "${VERDE}Configurando Polkit rule para Pamac${NC}"
+echo -e "${GREEN}Configurando Polkit rule para Pamac${NC}"
 if grep -q '^wheel:' /etc/group; then USER_GROUP="wheel"; else USER_GROUP="sudo"; fi
 
 sudo tee $PAMAC_RULE_PATH > /dev/null <<EOF
@@ -227,10 +302,19 @@ polkit.addRule(function(action, subject) {
 });
 EOF
 
-echo -e "${VERDE}Configurando ZRAM...${NC}"
+echo -e "${GREEN}Configurando ZRAM...${NC}"
 echo -e "[zram0]\nzram-size = ram\ncompression-algorithm = zstd" | sudo tee /etc/systemd/zram-generator.conf > /dev/null
 
-echo -e "${VERDE}Configurando Segurança e Habilitando Serviços...${NC}"
+if [ -n "$UV_VAL" ]; then
+    echo -e "${GREEN}Aplicando arquivo de configuração do Undervolt...${NC}"
+    sudo cp /etc/intel-undervolt.conf /etc/intel-undervolt.conf.bak
+    sudo sed -i "s/^undervolt 0.*/undervolt 0 'CPU' ${UV_VAL}/" /etc/intel-undervolt.conf
+    sudo sed -i "s/^undervolt 2.*/undervolt 2 'CPU Cache' ${UV_VAL}/" /etc/intel-undervolt.conf
+    sudo systemctl enable --now intel-undervolt.service
+    sudo intel-undervolt apply
+fi
+
+echo -e "${GREEN}Configurando Segurança e Habilitando Serviços...${NC}"
 # UFW e KDE Connect
 sudo systemctl enable --now ufw.service
 sudo ufw allow 1714:1764/udp
@@ -241,11 +325,12 @@ sudo ufw --force enable
 sudo systemctl daemon-reload
 sudo systemctl start systemd-zram-setup@zram0.service
 sudo systemctl enable --now switcheroo-control.service
+sudo systemctl enable --now tuned
 sudo systemctl enable --now fstrim.timer
 
-echo -e "${VERDE}Limpando arquivos temporários...${NC}"
+echo -e "${GREEN}Limpando arquivos temporários...${NC}"
 rm -rf "$CACHE"
-echo -e "${VERDE}------------------------------------------${NC}"
+echo -e "${GREEN}------------------------------------------${NC}"
 echo "Instalação finalizada."
 read -p "Deseja reiniciar o sistema? (s/n): " resposta
 
